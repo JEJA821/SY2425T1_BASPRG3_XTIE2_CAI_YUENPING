95fb474570d082f6e48a69f9d35dde3a38235a72
JEJA821 <84137174+JEJA821@users.noreply.github.com>
2024-09-30 17:04:01 +0800

    Create Player.h

---
+++ Milestone 1/Player.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "GameObject.h"
+#include "Common.h"
+#include "draw.h"
+
+class Player :
+    public GameObject
+{
+public:
+    void start();
+    void update();
+    void draw();
+private:
+    int x;
+    int y;
+    int width;
+    int height;
+    SDL_Texture* texture;
+
+private:
+    float originalSpeed;
+    float increasedSpeed;
+    float currentSpeed;
+
+};
